76481681cb75c5920b48b43e614db76f8371fa64
Phil <psnider85@users.noreply.github.com>
2014-10-27 23:06:33 +1000

    Added encoding/decoding of strings

--- src/com/ullarah/rotterbot/modules/LastFM.java
+++ src/com/ullarah/rotterbot/modules/LastFM.java
@@ -9,25 +9,30 @@ import org.json.simple.parser.ParseException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
 import java.net.URLConnection;
 
 import static com.ullarah.rotterbot.Client.pluginKey;
+import static com.ullarah.rotterbot.Utility.urlDecode;
+import static com.ullarah.rotterbot.Utility.urlEncode;
 
 public class LastFM {
 
     private static JSONObject getAPI(String type, String args) {
 
         try {
-            args = args.replaceAll(" ", "+");
-
             URL url = new URL("http://ws.audioscrobbler.com/2.0/?method=" + type + "&api_key=" + pluginKey("lastfm") + "&format=json" + args);
             URLConnection conn = url.openConnection();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             JSONParser jsonParser = new JSONParser();
 
-            return (JSONObject) jsonParser.parse(reader);
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+
+            reader.close();
+
+            return jsonObject;
         } catch (IOException | ParseException e) {
             e.printStackTrace();
         }
@@ -36,18 +41,18 @@ public class LastFM {
 
     }
 
-    public static String getUserTopTrack(String user) {
+    public static String getUserTopTrack(String user) throws UnsupportedEncodingException {
 
-        JSONObject lastFMObject = getAPI("user.gettoptracks", "&limit=1&user=" + user);
+        JSONObject lastFMObject = getAPI("user.gettoptracks", "&limit=1&user=" + urlEncode(user));
 
         JSONObject topTrackObject = (JSONObject) lastFMObject.get("toptracks");
         JSONObject trackObject = (JSONObject) topTrackObject.get("track");
 
         JSONObject trackArtistObject = (JSONObject) trackObject.get("artist");
 
-        String trackName = (String) trackObject.get("name");
-        String trackArtist = (String) trackArtistObject.get("name");
-        String trackPlays = (String) trackObject.get("playcount");
+        String trackName = urlDecode((String) trackObject.get("name"));
+        String trackArtist = urlDecode((String) trackArtistObject.get("name"));
+        String trackPlays = urlDecode((String) trackObject.get("playcount"));
 
         return "[LASTFM] Top Track: " +
                 Colour.BOLD + trackName + Colour.RESET +
@@ -56,9 +61,9 @@ public class LastFM {
 
     }
 
-    public static String getUserRecentTrack(String user) {
+    public static String getUserRecentTrack(String user) throws UnsupportedEncodingException {
 
-        JSONObject lastFMObject = getAPI("user.getrecenttracks", "&limit=1&user=" + user);
+        JSONObject lastFMObject = getAPI("user.getrecenttracks", "&limit=1&user=" + urlEncode(user));
 
         JSONObject topRecentTrackObject = (JSONObject) lastFMObject.get("recenttracks");
 
@@ -70,8 +75,8 @@ public class LastFM {
 
             JSONObject trackArtistObject = (JSONObject) trackJSONObject.get("artist");
 
-            String trackName = (String) trackJSONObject.get("name");
-            String trackArtist = (String) trackArtistObject.get("#text");
+            String trackName = urlDecode((String) trackJSONObject.get("name"));
+            String trackArtist = urlDecode((String) trackArtistObject.get("#text"));
 
             return "[LASTFM] Recent Track: " +
                     Colour.BOLD + trackName + Colour.RESET +
@@ -85,8 +90,8 @@ public class LastFM {
 
             JSONObject trackArtistObject = (JSONObject) trackPlayObject.get("artist");
 
-            String trackName = (String) trackPlayObject.get("name");
-            String trackArtist = (String) trackArtistObject.get("#text");
+            String trackName = urlDecode((String) trackPlayObject.get("name"));
+            String trackArtist = urlDecode((String) trackArtistObject.get("#text"));
 
             return "[LASTFM] Playing Now: " +
                     Colour.BOLD + trackName + Colour.RESET +
@@ -100,11 +105,11 @@ public class LastFM {
 
     }
 
-    public static String getArtistSimilar(String artist) {
+    public static String getArtistSimilar(String artist) throws UnsupportedEncodingException {
 
         artist = artist.trim();
 
-        JSONObject lastFMObject = getAPI("artist.getSimilar", "&artist=" + artist);
+        JSONObject lastFMObject = getAPI("artist.getSimilar", "&artist=" + urlEncode(artist));
 
         if (lastFMObject.containsKey("similarartists")) {
 
@@ -116,21 +121,23 @@ public class LastFM {
 
                 JSONObject similarArtist = (JSONObject) similarArtists.get(randomSimilarArtist);
 
-                String artistName = (String) similarArtist.get("name");
+                String artistName = urlDecode((String) similarArtist.get("name"));
 
-                return "[LASTFM] Similar Artist to " + artist + ": " + Colour.BOLD + artistName;
-            } else return "[LASTFM] No similar artists to: " + Colour.BOLD + artist;
+                return "[LASTFM] Similar Artist to " + urlDecode(artist) + ": " + Colour.BOLD + urlDecode(artistName);
+            } else return "[LASTFM] No similar artists to: " + Colour.BOLD + urlDecode(artist);
 
-        } else return "[LASTFM] No similar artists to: " + Colour.BOLD + artist;
+        } else return "[LASTFM] No similar artists to: " + Colour.BOLD + urlDecode(artist);
 
     }
 
-    public static String getTrackSimilar(String artist, String track) {
+    public static String getTrackSimilar(String artist, String track) throws UnsupportedEncodingException {
 
         artist = artist.trim();
         track = track.trim();
 
-        JSONObject lastFMObject = getAPI("track.getSimilar", "&artist=" + artist + "&track=" + track);
+        JSONObject lastFMObject = getAPI("track.getSimilar",
+                "&artist=" + urlEncode(artist) +
+                        "&track=" + urlEncode(track));
 
         if (lastFMObject.containsKey("similartracks")) {
 
@@ -143,20 +150,20 @@ public class LastFM {
 
             JSONObject trackArtistObject = (JSONObject) similarTrack.get("artist");
 
-            String trackName = (String) similarTrack.get("name");
-            String trackArtist = (String) trackArtistObject.get("name");
+            String trackName = urlDecode((String) similarTrack.get("name"));
+            String trackArtist = urlDecode((String) trackArtistObject.get("name"));
 
             return "[LASTFM] Similar Track to " + artist + " by " + track + ": " +
                     Colour.BOLD + trackName + Colour.RESET +
                     " by " + Colour.BOLD + trackArtist;
 
         } else return "[LASTFM] No similar tracks to:" +
-                Colour.BOLD + artist + Colour.RESET +
-                " by " + Colour.BOLD + track;
+                Colour.BOLD + urlDecode(artist) + Colour.RESET +
+                " by " + Colour.BOLD + urlDecode(track);
 
     }
 
-    public static String getHypedTrack() {
+    public static String getHypedTrack() throws UnsupportedEncodingException {
 
         JSONObject lastFMObject = getAPI("chart.gethypedtracks", "&limit=1");
 
@@ -165,8 +172,8 @@ public class LastFM {
 
         JSONObject trackArtistObject = (JSONObject) trackObject.get("artist");
 
-        String trackName = (String) trackObject.get("name");
-        String trackArtist = (String) trackArtistObject.get("name");
+        String trackName = urlDecode((String) trackObject.get("name"));
+        String trackArtist = urlDecode((String) trackArtistObject.get("name"));
 
         return "[LASTFM] Hyped Chart Track: " +
                 Colour.BOLD + trackName + Colour.RESET +
@@ -174,7 +181,7 @@ public class LastFM {
 
     }
 
-    public static String getTopTrack() {
+    public static String getTopTrack() throws UnsupportedEncodingException {
 
         JSONObject lastFMObject = getAPI("chart.gettoptracks", "&limit=1");
 
@@ -183,8 +190,8 @@ public class LastFM {
 
         JSONObject trackArtistObject = (JSONObject) trackObject.get("artist");
 
-        String trackName = (String) trackObject.get("name");
-        String trackArtist = (String) trackArtistObject.get("name");
+        String trackName = urlDecode((String) trackObject.get("name"));
+        String trackArtist = urlDecode((String) trackArtistObject.get("name"));
 
         return "[LASTFM] Top Chart Track: " +
                 Colour.BOLD + trackName + Colour.RESET +
